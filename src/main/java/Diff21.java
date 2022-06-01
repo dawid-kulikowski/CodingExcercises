b8da44f41e82fd6993c33a69ca2eaccc535d139f
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-01 07:29:15 +0200

    CodingBat - Diff21

---
+++ src/main/java/Diff21.java
@@ -0,0 +1,27 @@
+//
+//Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+//
+//
+//        diff21(19) → 2
+//        diff21(10) → 11
+//        diff21(21) → 0
+
+
+public class Diff21 {
+    public Diff21(int n) {
+        this.n = n;
+    }
+
+    private int n;
+
+    public int diff21(int n) {
+        if (n<21) {
+            return 21-n;
+        } else if (n>21) {
+            return (2*(n-21));
+        } else {
+            return 0;
+        }
+    }
+
+}
