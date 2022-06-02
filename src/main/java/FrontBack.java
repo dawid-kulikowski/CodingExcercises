905f40881a948670dcfa98ab4a4350e02ce39ff2
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-02 21:23:21 +0200

    CodingBat - Front3

--- src/main/java/FrontBack.java
+++ src/main/java/FrontBack.java
@@ -1,3 +1,11 @@
+//Given a string, return a new string where the first and last chars have been exchanged.
+//
+//
+//        frontBack("code") → "eodc"
+//        frontBack("a") → "a"
+//        frontBack("ab") → "ba"
+//
+
 public class FrontBack {
     String str;
 
