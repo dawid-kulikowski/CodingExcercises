fc691e580b07bff886999cedd75bde0372bcea50
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-02 21:06:43 +0200

    CodingBat - FrontBack

---
+++ src/main/java/FrontBack.java
@@ -0,0 +1,19 @@
+public class FrontBack {
+    String str;
+
+    public FrontBack(String str) {
+        this.str = str;
+    }
+
+    public String frontBack(String str) {
+        if (str.length()<=1) {
+            return str;
+        }
+
+        String mid = str.substring(1,str.length()-1);
+
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    }
+
+
+}
