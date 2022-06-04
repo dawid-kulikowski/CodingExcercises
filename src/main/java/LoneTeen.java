9a64d44f1a722cfdd70c46d00d745cd4be7882c4
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-04 19:10:22 +0200

    CodingBat - LoneTeen

---
+++ src/main/java/LoneTeen.java
@@ -0,0 +1,34 @@
+//
+//We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+//
+//
+//        loneTeen(13, 99) → true
+//        loneTeen(21, 19) → true
+//        loneTeen(13, 13) → false
+
+
+public class LoneTeen {
+ private int a;
+ private int b;
+
+    public LoneTeen(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public boolean loneTeen(int a, int b) {
+
+        if ((a>=13 && a<=19) && (b>=13 && b<=19)) {
+            return false;
+        }
+        else if ((a>=13 && a<=19) || (b>=13 && b<=19)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+}
