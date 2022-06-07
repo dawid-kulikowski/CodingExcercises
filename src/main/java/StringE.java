357227fe79574a01b66d71adafa3565f236e6fee
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-07 17:53:27 +0200

    CodingBat - StringE

---
+++ src/main/java/StringE.java
@@ -0,0 +1,24 @@
+
+//Return true if the given string contains between 1 and 3 'e' chars.
+//
+//
+//        stringE("Hello") → true
+//        stringE("Heelle") → true
+//        stringE("Heelele") → false
+
+public class StringE {
+    public StringE(String str) {
+        this.str = str;
+    }
+
+    private String str;
+
+    public boolean stringE(String str) {
+        int count = 0;
+
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i) == 'e') count++;
+        }
+        return (count >= 1 && count <= 3);
+    }
+}
